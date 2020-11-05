0bd85a62472d1b89f47bd2d664e58d188c5de2e6
Maksym Zaporozhets <maksimz@default-value.com>
2020-11-05 18:58:58 +0200

    #5 LESS and RWD: improved the script for local deployment

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-BRANCH='lesson-4-magento-upgrade-and-creating-theme'
+BRANCH='lesson-5-less'
 
-php bin/magento deploy:mode:set default
 git checkout $BRANCH
 git merge origin/$BRANCH
+php bin/magento deploy:mode:set default
 composer install
 rm -rf var/cache/* var/page_cache/* var/view_preprocessed/* pub/static/frontend/* pub/static/adminhtml/* pub/static/_cache/* pub/static/deployed_version.txt generated/code/* generated/metadata/*
 php bin/magento setup:upgrade
