773b78c8fecff66ea23e27e26c62ccdb5f46188a
Maksym Zaporozhets <maksimz@default-value.com>
2020-12-10 14:13:22 +0200

    #10 Schema and data installation: added schema patch to remove column `extra_demo_column` the table `dv_campus_personal_discount_request`

---
+++ app/code/DVCampus/PersonalDiscount/Setup/Patch/Schema/DropExtraDemoColumn.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+
+namespace DVCampus\PersonalDiscount\Setup\Patch\Schema;
+
+class DropExtraDemoColumn implements \Magento\Framework\Setup\Patch\SchemaPatchInterface
+{
+    /**
+     * @var \Magento\Framework\Setup\ModuleDataSetupInterface $schemaSetup
+     */
+    private $schemaSetup;
+
+    /**
+     * RemoveOldForeignKeys constructor.
+     * @param \Magento\Framework\Setup\SchemaSetupInterface $schemaSetup
+     */
+    public function __construct(
+        \Magento\Framework\Setup\SchemaSetupInterface $schemaSetup
+    ) {
+        $this->schemaSetup = $schemaSetup;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function apply(): void
+    {
+        $this->schemaSetup->getConnection()
+            ->dropColumn(
+                $this->schemaSetup->getTable('dv_campus_personal_discount_request'),
+                'extra_demo_column'
+            );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function getDependencies(): array
+    {
+        return [];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getAliases(): array
+    {
+        return [];
+    }
+}
