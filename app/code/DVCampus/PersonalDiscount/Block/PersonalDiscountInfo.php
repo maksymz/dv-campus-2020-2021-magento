06a476f5d8f0c04099fed893b76c2f9421f3e6d2
Maksym Zapoorozhets <maksimzaporozhets@gmail.com>
2020-12-17 01:18:35 +0200

    #12 Model, Resource Model, Collection: showing discount request in My Account

---
+++ app/code/DVCampus/PersonalDiscount/Block/PersonalDiscountInfo.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DVCampus\PersonalDiscount\Block;
+
+use DVCampus\PersonalDiscount\Model\DiscountRequest;
+use DVCampus\PersonalDiscount\Model\ResourceModel\DiscountRequest\Collection as DiscountRequestCollection;
+use Magento\Framework\Phrase;
+
+class PersonalDiscountInfo extends \Magento\Framework\View\Element\Template
+{
+    /**
+     * @var \DVCampus\PersonalDiscount\Model\ResourceModel\DiscountRequest\CollectionFactory $collectionFactorys
+     */
+    private $collectionFactory;
+
+    /**
+     * @var \Magento\Store\Model\StoreManagerInterface $storeManager
+     */
+    private $storeManager;
+
+    /**
+     * PersonalDiscountInfo constructor.
+     * @param \DVCampus\PersonalDiscount\Model\ResourceModel\DiscountRequest\CollectionFactory $collectionFactory
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param array $data
+     */
+    public function __construct(
+        \DVCampus\PersonalDiscount\Model\ResourceModel\DiscountRequest\CollectionFactory $collectionFactory,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Framework\View\Element\Template\Context $context,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->collectionFactory = $collectionFactory;
+        $this->storeManager = $storeManager;
+    }
+
+    /**
+     * @return DiscountRequest|null
+     */
+    public function getPersonalDiscount(): ?DiscountRequest
+    {
+        /** @var DiscountRequestCollection $collection */
+        $collection = $this->collectionFactory->create();
+        // @TODO: get current customer's ID
+        $collection->addFieldToFilter('email', 'john-doe@example.com');
+        // @TODO: check if accounts are shared or not
+        $collection->addFieldToFilter('website_id', $this->storeManager->getStore()->getWebsiteId());
+        /** @var DiscountRequest $discountRequest */
+        $discountRequest = $collection->getFirstItem();
+
+        return $discountRequest->getDiscountRequestId() ? $discountRequest : null;
+    }
+
+    /**
+     * @param DiscountRequest $discountRequest
+     * @return Phrase
+     */
+    public function getStatusMessage(DiscountRequest $discountRequest): Phrase
+    {
+        switch ($discountRequest->getStatus()) {
+            case DiscountRequest::STATUS_PENDING:
+                return __('pending');
+            case DiscountRequest::STATUS_APPROVED:
+                return __('approved');
+            case DiscountRequest::STATUS_DECLINED:
+            default:
+                return __('declined');
+        }
+    }
+}
