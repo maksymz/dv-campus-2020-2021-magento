8a4a0f7a2faa1135fb1f5a94e89d97b5814b16ef
Maksym Zapoorozhets <maksimzaporozhets@gmail.com>
2020-12-17 00:23:14 +0200

    #12 Model, Resource Model, Collection: added model and resource model, saving data to database in controller.

--- app/code/DVCampus/PersonalDiscount/Controller/Index/Request.php
+++ app/code/DVCampus/PersonalDiscount/Controller/Index/Request.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace DVCampus\PersonalDiscount\Controller\Index;
 
 use Magento\Framework\Controller\Result\Json as JsonResponse;
+use DVCampus\PersonalDiscount\Model\DiscountRequest;
 
 class Request implements \Magento\Framework\App\Action\HttpPostActionInterface
 {
@@ -19,16 +20,48 @@ class Request implements \Magento\Framework\App\Action\HttpPostActionInterface
     private $jsonResponseFactory;
 
     /**
+     * @var \DVCampus\PersonalDiscount\Model\DiscountRequestFactory $discountRequestFactory
+     */
+    private $discountRequestFactory;
+
+    /**
+     * @var \Magento\Store\Model\StoreManagerInterface $storeManager
+     */
+    private $storeManager;
+
+    /**
+     * @var \DVCampus\PersonalDiscount\Model\ResourceModel\DiscountRequest $discountRequestResource
+     */
+    private $discountRequestResource;
+
+    /**
+     * @var \Psr\Log\LoggerInterface $logger
+     */
+    private $logger;
+
+    /**
      * Controller constructor.
      * @param \Magento\Framework\App\RequestInterface $request
      * @param \Magento\Framework\Controller\Result\JsonFactory $jsonResponseFactory
+     * @param \DVCampus\PersonalDiscount\Model\DiscountRequestFactory $discountRequestFactory
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param \DVCampus\PersonalDiscount\Model\ResourceModel\DiscountRequest $discountRequestResource
+     * @param \Psr\Log\LoggerInterface $logger
      */
     public function __construct(
         \Magento\Framework\App\RequestInterface $request,
-        \Magento\Framework\Controller\Result\JsonFactory $jsonResponseFactory
+        \Magento\Framework\Controller\Result\JsonFactory $jsonResponseFactory,
+        \DVCampus\PersonalDiscount\Model\DiscountRequestFactory $discountRequestFactory,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \DVCampus\PersonalDiscount\Model\ResourceModel\DiscountRequest $discountRequestResource,
+        \Psr\Log\LoggerInterface $logger
     ) {
         $this->request = $request;
         $this->jsonResponseFactory = $jsonResponseFactory;
+        $this->discountRequestFactory = $discountRequestFactory;
+        $this->storeManager = $storeManager;
+        $this->discountRequestResource = $discountRequestResource;
+        $this->logger = $logger;
     }
 
     /**
@@ -42,10 +75,24 @@ class Request implements \Magento\Framework\App\Action\HttpPostActionInterface
         // @TODO: pass message via notifications, not alert
         // @TODO: add form key validation and hideIt validation
         // @TODO: add Google Recaptcha to the form
-        $response->setData([
-            'message' => __('You request for product %1 was accepted!', $this->request->getParam('productName'))
-        ]);
 
-        return $response;
+        try {
+            /** @var DiscountRequest $discountRequest */
+            $discountRequest = $this->discountRequestFactory->create();
+            $discountRequest->setName($this->request->getParam('name'))
+                ->setEmail($this->request->getParam('email'))
+                ->setMessage($this->request->getParam('email'))
+                ->setWebsiteId($this->storeManager->getStore()->getWebsiteId())
+                ->setStatus($this->request->getParam('email'));
+            $this->discountRequestResource->save($discountRequest);
+            $message = __('You request for product %1 was accepted!', $this->request->getParam('productName'));
+        } catch (\Exception $e) {
+            $this->logger->error($e->getMessage());
+            $message = __('Your request can\'t be sent. Please, contact us if you see this message.');
+        }
+
+        return $response->setData([
+            'message' => $message
+        ]);
     }
 }
---
+++ app/code/DVCampus/PersonalDiscount/Model/DiscountRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DVCampus\PersonalDiscount\Model;
+
+/**
+ * @method int|string|null getDiscountRequestId()
+ * @method int|string|null getCustomerId()
+ * @method $this setCustomerId(int $customerId)
+ * @method string|null getName()
+ * @method $this setName(string $name)
+ * @method string|null getEmail()
+ * @method $this setEmail(string $name)
+ * @method string|null getMessage()
+ * @method $this setMessage(string $message)
+ * @method int|string|null getWebsiteId()
+ * @method $this setWebsiteId(int $websiteId)
+ * @method int|string|null getStatus()
+ * @method $this setStatus(int $status)
+ * @method int|string|null getCreatedAt()
+ * @method int|string|null getUpdatedAt()
+ */
+class DiscountRequest extends \Magento\Framework\Model\AbstractModel
+{
+    public const STATUS_PENDING = 1;
+    public const STATUS_APPROVED = 2;
+    public const STATUS_DECLINED = 3;
+
+    /**
+     * @inheritDoc
+     */
+    protected function _construct(): void
+    {
+        parent::_construct();
+        $this->_init(\DVCampus\PersonalDiscount\Model\ResourceModel\DiscountRequest::class);
+    }
+}
