03abcea64ea22662d1677be8d1bf85b2ffaeeec2
Maksym Zaporozhets <maksimz@default-value.com>
2021-01-21 14:56:46 +0200

    #15 Knockout.js, UI Components: Extracted form submit into a separate action script

--- app/code/DVCampus/PersonalDiscount/view/frontend/web/js/view/form.js
+++ app/code/DVCampus/PersonalDiscount/view/frontend/web/js/view/form.js
@@ -3,11 +3,10 @@ define([
     'ko',
     'uiComponent',
     'Magento_Customer/js/customer-data',
-    'Magento_Ui/js/modal/alert',
+    'dvCampusPersonalDiscountSubmitForm',
     'Magento_Ui/js/modal/modal',
-    'mage/translate',
     'mage/cookies'
-], function ($, ko, Component, customerData, alert) {
+], function ($, ko, Component, customerData, submitFormAction) {
     'use strict';
 
     return Component.extend({
@@ -97,41 +96,10 @@ define([
                 isAjax: 1
             };
 
-            $.ajax({
-                url: this.action,
-                data: payload,
-                type: 'post',
-                dataType: 'json',
-                context: this,
-
-                /** @inheritdoc */
-                beforeSend: function () {
-                    $('body').trigger('processStart');
-                },
-
-                /** @inheritdoc */
-                success: function (response) {
-                    alert({
-                        title: $.mage.__('Success'),
-                        content: response.message
-                    });
-                },
-
-                /** @inheritdoc */
-                error: function () {
-                    alert({
-                        title: $.mage.__('Error'),
-                        /*eslint max-len: ["error", { "ignoreStrings": true }]*/
-                        content: $.mage.__('Your request can\'t be sent. Please, contact us if you see this message.')
-                    });
-                },
-
-                /** @inheritdoc */
-                complete: function () {
-                    $('body').trigger('processStop');
+            submitFormAction(payload, this.action)
+                .done(function () {
                     this.$modal.modal('closeModal');
-                }
-            });
+                }.bind(this));
         }
     });
 });
